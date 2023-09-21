0fdd9f0d7459e3d979fd3d9f9bdbe5f23760dd5f
Gertrude Seworh <111208672+kaf0@users.noreply.github.com>
2023-09-21 11:12:22 +0000

    about

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,53 @@
+/*
+  Authors : initappz (Rahul Jograna)
+  Website : https://initappz.com/
+  App Name : Ultimate Salon Full App Flutter
+  This App Template Source code is licensed as per the
+  terms found in the Website https://initappz.com/license
+  Copyright and Good Faith Purchasers © 2022-present initappz.
+*/
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { UtilService } from 'src/app/services/util.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+  content: any;
+  loaded: boolean;
+  constructor(
+    public api: ApiService,
+    public util: UtilService
+  ) {
+    this.loaded = false;
+    this.getPageInfo();
+  }
+
+  getPageInfo() {
+    const param = {
+      id: 1
+    };
+    console.log('param', param);
+    this.api.post_private('v1/pages/getContent', param).then((data: any) => {
+      this.loaded = true;
+      console.log(data);
+      if (data && data.status && data.status == 200 && data.data) {
+        this.content = data.data.content;
+      }
+    }, error => {
+      console.log(error);
+      this.util.errorMessage(this.util.translate('Something went wrong'));
+      this.loaded = true;
+    }).catch(error => {
+      console.log(error);
+      this.util.errorMessage(this.util.translate('Something went wrong'));
+      this.loaded = true;
+    });
+  }
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/about/about.module.ts
@@ -0,0 +1,26 @@
+/*
+  Authors : initappz (Rahul Jograna)
+  Website : https://initappz.com/
+  App Name : Ultimate Salon Full App Flutter
+  This App Template Source code is licensed as per the
+  terms found in the Website https://initappz.com/license
+  Copyright and Good Faith Purchasers © 2022-present initappz.
+*/
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AboutRoutingModule } from './about-routing.module';
+import { AboutComponent } from './about.component';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+
+@NgModule({
+  declarations: [
+    AboutComponent
+  ],
+  imports: [
+    CommonModule,
+    AboutRoutingModule,
+    NgxSkeletonLoaderModule
+  ]
+})
+export class AboutModule { }
